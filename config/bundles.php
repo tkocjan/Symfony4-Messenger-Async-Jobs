4c432d6c083abb33ae9ef09016b23bf147a7fe33
Tom Kocjan <tkocjan@i3logix.com>
2018-12-21 17:33:15 -0500

    add_enqueue: converted to 4.2.1 and added enqueue

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Enqueue\Bundle\EnqueueBundle::class => ['all' => true],
+    Enqueue\MessengerAdapter\Bundle\EnqueueAdapterBundle::class => ['all' => true],
 ];
--- src/Core/Application/MessageHandler/CreateSalesOrder2Handler.php
+++ src/Core/Application/MessageHandler/CreateSalesOrder2Handler.php
@@ -4,12 +4,12 @@ namespace App\Core\Application\MessageHandler;
 use App\Core\Domain\Message\CreateSalesOrder;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-final class CreateSalesOrderHandler implements MessageHandlerInterface
+final class CreateSalesOrder2Handler implements MessageHandlerInterface
 {
     public function __invoke(CreateSalesOrder $createSalesOrder)
     {
         // Todo: Create Purchase order
-        echo 'CreateSalesOrderHandler';
+        echo 'CreateSalesOrder2Handler';
         var_dump($createSalesOrder);
     }
 }
